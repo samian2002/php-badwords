b08096a8d8f9f163c8fb7fd4c16306e08353fc97
samian2002 <samiantariq14@gmail.com>
2024-06-10 14:49:44 +0200

    first commit

---
+++ form.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Invia un paragrafo</title>
+</head>
+<body>
+    <form action="process.php" method="post">
+        Inserisci il tuo paragrafo:<br>
+        <textarea name="paragrafo" rows="10" cols="50"></textarea><br>
+        <input type="submit" value="Invia">
+    </form>
+</body>
+</html>
---
+++ process.php
@@ -0,0 +1,5 @@
+<?php
+$paragrafo = $_POST['paragrafo'];
+$lunghezza = strlen($paragrafo);
+echo "Il paragrafo inviato è lungo $lunghezza caratteri.";
+?>
